2bd70d282254f6bdefaa048f936944bda0273dd8
Tim McGinley <timwasnever@gmail.com>
2023-08-30 12:50:40 +0200

    remove basic_function

--- Examples/Python/basic_function/file_open.py
+++
@@ -1,6 +0,0 @@
-from Tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
