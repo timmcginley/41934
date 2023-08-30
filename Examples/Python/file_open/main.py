9720be400ffa9b94bbbaa0d22d28185e1f6d40da
Tim McGinley <timwasnever@gmail.com>
2023-08-30 12:52:07 +0200

    remove file_open

--- Examples/Python/file_open/main.py
+++
@@ -1,8 +0,0 @@
-# thanks to https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
-
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
