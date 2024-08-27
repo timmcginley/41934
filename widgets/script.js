e1147b551e5763ace07de56b0f5e1cb4f30c2e04
Kaare Hansen <kaare@gshansen.dk>
2024-08-27 14:47:25 +0200

    Remove Jekyll stuff

--- widgets/script.js
+++
@@ -1,9 +0,0 @@
-
-<script>
-jQuery.get("https://raw.githubusercontent.com/DTU-Byg/11034/main/A1__Dashboard/readme.md", function(data) {
-	var converter = new showdown.Converter(),
-    html      = converter.makeHtml(data);
-  	$( "#para" ).html( html );
-  alert( "Load was performed." );
-});
-</script>
